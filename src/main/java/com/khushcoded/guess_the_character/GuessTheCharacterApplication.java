58c8a76016b2f36fd045881bf7b00449823296eb
khushboo choudhary <ckhushboo483@gmail.com>
2026-08-21 12:30:01 +0530

    chore: initialize spring boot project

---
+++ src/main/java/com/khushcoded/guess_the_character/GuessTheCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.khushcoded.guess_the_character;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessTheCharacterApplication.class, args);
+	}
+
+}
